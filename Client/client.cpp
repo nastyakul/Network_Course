083a3f28674f016b56b1ab8605443e83fa7d1036
EliseRybka <35104057+EliseRybka@users.noreply.github.com>
2018-05-17 18:50:17 +0300

    elise client.cpp

--- Client/client.cpp
+++ Client/client.cpp
@@ -6,35 +6,36 @@
 
 using namespace std;
 
-static const char exit_phrase[] = "Terminate RCP"; //stop connection 
+static const char exit_phrase[] = "Terminate RCP";    //на удаленном компьютере открывается сервер на прослушуку. 
+                                                       //Эта фраза прерывает и все осторожно выключает (цикл while 1)
 
 #pragma comment (lib, "Ws2_32.lib")
 
 #define DEFAULT_BUFLEN 4096
-#define HOST "localhost" //127.0.0.1
-#define DEFAULT_PORT "3504" //any number, for ex. http port 80
+#define HOST "localhost"                               //DNS -> 127.0.0.1
+#define DEFAULT_PORT "3504"                             // http = 80; 
 
 #define EXIT_PROGRAMM(RETURN_VALUE) \
         system("pause");\
         return RETURN_VALUE;
 
-struct client_type //client_connection
+struct client_type
 {
-        SOCKET socket; 
+        SOCKET socket;
         char received_message[DEFAULT_BUFLEN];
 };
 
-int process_client(client_type &new_client) //ссылка на структуру, вот начинается функция!
+int process_client(client_type &new_client)    //ссылка на структуру
 {
         while (1)
         {
-                memset(new_client.received_message, 0, DEFAULT_BUFLEN); //установи столько байтов в буфлене в нули
+                memset(new_client.received_message, 0, DEFAULT_BUFLEN);  //установи DEFAULT_BUFLEN байт там в нули 
 
-                if (new_client.socket != 0) //если есть сокет
+                if (new_client.socket != 0)
                 {
-                        int iResult = recv(new_client.socket, new_client.received_message, DEFAULT_BUFLEN, 0); //откуда, куда записать
-                                                                                                               //возвращает число 
-                        if (iResult != SOCKET_ERROR)                      //вывод                             //байт
+                        int iResult = recv(new_client.socket, new_client.received_message, DEFAULT_BUFLEN, 0);
+
+                        if (iResult != SOCKET_ERROR)
                                 cout << new_client.received_message;
                         else
                         {
@@ -44,7 +45,7 @@ int process_client(client_type &new_client) //ссылка на структур
                 }
         }
 
-        if (WSAGetLastError() == WSAECONNRESET)         //особая ошибочка
+        if (WSAGetLastError() == WSAECONNRESET)
                 cout << "The server has disconnected" << endl;
 
         return 0;
@@ -53,21 +54,21 @@ int process_client(client_type &new_client) //ссылка на структур
 int main()
 {
         WSAData wsa_data;
-        struct addrinfo *result = NULL, *ptr = NULL, hints; //два указателя на структуру и структура
-        client_type client = { INVALID_SOCKET, "" }; //инициализация
+        struct addrinfo *result = NULL, *ptr = NULL, hints;
+        client_type client = { INVALID_SOCKET, "" };
         int iResult = 0;
         string message;
 
         cout << "Starting Client...\n";
 
         // Initialize Winsock
-        iResult = WSAStartup(MAKEWORD(2, 2), &wsa_data); //инициализируем служебное, почитать
+        iResult = WSAStartup(MAKEWORD(2, 2), &wsa_data);
         if (iResult != 0) {
                 cout << "WSAStartup() failed with error: " << iResult << endl;
                 EXIT_PROGRAMM(1);
         }
 
-        ZeroMemory(&hints, sizeof(hints)); //обнуляет память
+        ZeroMemory(&hints, sizeof(hints));
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_protocol = IPPROTO_TCP;
@@ -75,26 +76,26 @@ int main()
         cout << "Connecting...\n";
 
         // Resolve the server address and port
-        iResult = getaddrinfo(HOST, DEFAULT_PORT, &hints, &result); //указ на структуру и на указатель на указатель
-        if (iResult != 0) {                     //заполняем исходя из хоста и порта
+        iResult = getaddrinfo(HOST, DEFAULT_PORT, &hints, &result);   //инициализируем стурктуру для доступа к серверу
+        if (iResult != 0) {
                 cout << "getaddrinfo() failed with error: " << iResult << endl;
                 WSACleanup();
                 EXIT_PROGRAMM(1);
         }
 
         // Attempt to connect to an address until one succeeds
-        for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {   //iterate linked list
+        for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
 
                 // Create a SOCKET for connecting to server
-                client.socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol); //какой протокол использовать
+                client.socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
                 if (client.socket == INVALID_SOCKET) {
                         cout << "socket() failed with error: " << WSAGetLastError() << endl;
-                        WSACleanup();   //почисть
+                        WSACleanup();
                         EXIT_PROGRAMM(1);
                 }
 
                 // Connect to server.
-                iResult = connect(client.socket, ptr->ai_addr, (int)ptr->ai_addrlen); //коннектимся к сокету по адресу
+                iResult = connect(client.socket, ptr->ai_addr, (int)ptr->ai_addrlen);
                 if (iResult == SOCKET_ERROR) {
                         closesocket(client.socket);
                         client.socket = INVALID_SOCKET;
@@ -113,18 +114,18 @@ int main()
 
         cout << "Successfully Connected" << endl;
 
-        bool bSuccess;          //буффер для чтения с клавы
-        char chBuf[DEFAULT_BUFLEN]; //для чтения сообщений
-        DWORD dwRead, dwWritten; //дабл ворд 4 байта
-        HANDLE hParentStdIn = GetStdHandle(STD_INPUT_HANDLE); //для ввода
+        bool bSuccess;
+        char chBuf[DEFAULT_BUFLEN];
+        DWORD dwRead, dwWritten;                                           //каждая переменная по 2*2=4 байта
+        HANDLE hParentStdIn = GetStdHandle(STD_INPUT_HANDLE);              // обработчик для stdin
 
-        thread my_thread(process_client, client);  //потоки
+        thread my_thread(process_client, client);                          //запускает в потоке функцию process_client принимающую client
 
         while (1)
         {
-                bSuccess = ReadFile(hParentStdIn, chBuf, DEFAULT_BUFLEN, &dwRead, NULL); //из консоли в чибуф, куда прочитали
+                bSuccess = ReadFile(hParentStdIn, chBuf, DEFAULT_BUFLEN, &dwRead, NULL);   //примерно: stdin открыли как файл
 
-                iResult = send(client.socket, chBuf, dwRead, 0); //куда, что, сколько
+                iResult = send(client.socket, chBuf, dwRead, 0);   //клиент пишет серверу
 
                 if (iResult <= 0)
                 {
@@ -132,7 +133,7 @@ int main()
                         break;
                 }
 
-                if (strncmp(chBuf, exit_phrase, strlen(exit_phrase) - 1) == 0) //сравни строчки
+                if (strncmp(chBuf, exit_phrase, strlen(exit_phrase) - 1) == 0)
                         break;
         }
 
