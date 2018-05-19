8fc9a859ab5fe7b73ec501d365a96164e350fa13
Anastasiya Kulakova <nastyakul1997@gmail.com>
2018-05-19 14:40:50 +0300

    12

--- Server/server.cpp
+++ Server/server.cpp
@@ -15,88 +15,96 @@ const char OPTION_VALUE = 1;
 
 void delete_disconnected_clients(std::list<client_type*> *connected_clients) //указатель на список из подсоединенных клиентов
 {
-        std::list<client_type*> ::iterator iter = connected_clients -> begin();
-        while(iter != connected_clients -> end())  //итератор, класс, по которому можно итерироваться
-        {
-                if ((*iter) -> socket == INVALID_SOCKET) //разыменование, обращение в сокет
-                {
-                        delete (*iter)->proc;
-                        delete *iter;
-                        iter = connected_clients->erase(iter);
-                        continue;
-                }
-                iter++;
-        }
+	std::list<client_type*> ::iterator iter = connected_clients->begin();
+	while (iter != connected_clients->end())  //итератор, класс, по которому можно итерироваться
+	{
+		if ((*iter)->socket == INVALID_SOCKET) //разыменование, обращение в сокет
+		{
+			delete (*iter)->proc;
+			delete *iter;
+			iter = connected_clients->erase(iter);
+			continue;
+		}
+		iter++;
+	}
 }
 
 int main()
 {
-        WSADATA wsaData;
-        struct addrinfo hints;
-        struct addrinfo *server = NULL;
-        SOCKET server_socket = INVALID_SOCKET;
+	WSADATA wsaData;
+	struct addrinfo hints;
+	struct addrinfo *server = NULL;
+	SOCKET server_socket = INVALID_SOCKET;
+	std::list<client_type*> connected_clients;
 
-        std::list<client_type*> connected_clients;
+	//Initialize Winsock
+	std::cout << "Intializing Winsock..." << std::endl;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-        //Initialize Winsock
-        std::cout << "Intializing Winsock..." << std::endl;
-        WSAStartup(MAKEWORD(2, 2), &wsaData);
+	//Setup hints
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
 
-        //Setup hints
-        ZeroMemory(&hints, sizeof(hints));
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_protocol = IPPROTO_TCP;
-        hints.ai_flags = AI_PASSIVE;
+	//Setup Server
+	std::cout << "Setting up server..." << std::endl;
+	getaddrinfo("169.254.244.151", DEFAULT_PORT, &hints, &server); //
 
-        //Setup Server
-        std::cout << "Setting up server..." << std::endl;
-        getaddrinfo("localhost", DEFAULT_PORT, &hints, &server);
+	//Create a listening socket for connecting to server
+	std::cout << "Creating server socket..." << std::endl;
+	server_socket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
 
-        //Create a listening socket for connecting to server
-        std::cout << "Creating server socket..." << std::endl;
-        server_socket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
-
-        //Setup socket options
-        setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &OPTION_VALUE, sizeof(int)); 
-        setsockopt(server_socket, IPPROTO_TCP, TCP_NODELAY, &OPTION_VALUE, sizeof(int)); 
-
-                                                                                                       
-        std::cout << "Binding socket..." << std::endl;
-        bind(server_socket, server->ai_addr, (int)server->ai_addrlen);
-
-        //Listen for incoming connections.
-        std::cout << "Listening..." << std::endl;
-        listen(server_socket, SOMAXCONN); //читай документацию
-
-        while (1)
-        {
-                SOCKET incoming = INVALID_SOCKET;
-                incoming = accept(server_socket, NULL, NULL); //почитать
-
-                if (incoming == INVALID_SOCKET) continue;
-
-                //Create a temporary id for the next client
-
-                client_type* new_client = new client_type;  //new - это выделение памяти
-
-                new_client -> socket = incoming;
-
-                new_client -> proc = new m_process(); 
-                new_client -> m_threads[READ_PIPE_THREAD] = std::thread(ReadFromPipe, new_client); //0
-                new_client -> m_threads[WRITE_PIPE_THREAD] = std::thread(WriteToPipe, new_client);  //1
-                delete_disconnected_clients(&connected_clients);
-                connected_clients.push_back(new_client);
-        }
+	//Setup socket options
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &OPTION_VALUE, sizeof(int));
+	setsockopt(server_socket, IPPROTO_TCP, TCP_NODELAY, &OPTION_VALUE, sizeof(int));
 
 
-          //Close listening socket
-        closesocket(server_socket);
+	std::cout << "Binding socket..." << std::endl;
+	bind(server_socket, server->ai_addr, (int)server->ai_addrlen);
 
-        //Clean up Winsock
-        WSACleanup();
-        std::cout << "Program has ended successfully" << std::endl;
+	//Listen for incoming connections.
+	std::cout << "Listening..." << std::endl;
+	listen(server_socket, SOMAXCONN); //читай документацию
 
-        system("pause");
-        return 0;
+	while (1)
+	{
+
+		SOCKADDR_IN client_info = { 0 };
+		int addrsize = sizeof(client_info);
+
+		SOCKET incoming = INVALID_SOCKET;
+		incoming = accept(server_socket, (struct sockaddr*)&client_info, &addrsize); //почитать
+
+																					 // or get it from the socket itself at any time
+		addrsize = sizeof(client_info);
+		getpeername(incoming, (struct sockaddr*)&client_info, &addrsize);
+
+		char *ip = inet_ntoa(client_info.sin_addr);
+
+		printf("%s", ip);
+
+
+		client_type* new_client = new client_type;  //new - это выделение памяти
+
+		new_client->socket = incoming;
+
+		new_client->proc = new m_process();
+		new_client->m_threads[READ_PIPE_THREAD] = std::thread(ReadFromPipe, new_client); //0
+		new_client->m_threads[WRITE_PIPE_THREAD] = std::thread(WriteToPipe, new_client);  //1
+		delete_disconnected_clients(&connected_clients);
+		connected_clients.push_back(new_client);
+	}
+
+
+	//Close listening socket
+	closesocket(server_socket);
+
+	//Clean up Winsock
+	WSACleanup();
+	std::cout << "Program has ended successfully" << std::endl;
+
+	system("pause");
+	return 0;
 }
